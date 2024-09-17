855f4272a862cfa70679ffb80f096f33da89f611
Pamuda Sansika <113659841+pamusanzika@users.noreply.github.com>
2024-09-18 03:04:46 +0530

    Update weather_model.dart

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -3,17 +3,17 @@ class Weather {
   final double temperature;
   final String mainCondition;
 
-Weather ({
-  required this.cityName,
-  required this.mainCondition,
-  required this.temperature,
-});
+  Weather({
+    required this.cityName,
+    required this.mainCondition,
+    required this.temperature,
+  });
 
-factory Weather.fromjson(Map<String,dynamic > json){
-  return Weather(
-    cityName: json['name'],
-    temperature: json['main'] ['temp'].todouble(),
-    mainCondition: json['weather'] [0] ['main']);
-}
-
+  factory Weather.fromjson(Map<String, dynamic> json) {
+    return Weather(
+      cityName: json['name'],
+      temperature: (json['main']['temp'] as num).toDouble(), 
+      mainCondition: json['weather'][0]['main'],
+    );
+  }
 }
